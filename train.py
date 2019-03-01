3c3a9228343d6f21bd83bc2d1706c5cc27ad6e18
aisensiy <aisensiy@163.com>
2019-03-01 20:04:47 +0800

    feat: add customize metrics for keras

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 import argparse
 import os
 
+import openbayestool
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 parser = argparse.ArgumentParser(description="Process some integers.")
 parser.add_argument("-i", "--input", required=True, type=str, help="path for input data")
@@ -30,6 +32,14 @@ x_train = x_train.reshape(-1, img_rows * img_cols) / 255.0
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 
+class OpenBayesMetricsCallback(tf.keras.callbacks.Callback):
+    def on_batch_end(self, batch, logs=None):
+        """Print Training Metrics"""
+        if batch % 300 == 0:
+          openbayestool.log_metric('acc', float(logs.get('acc')))
+          openbayestool.log_metric('loss', float(logs.get('loss')))
+        
+
 def create_model():
   model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(512, activation=tf.nn.relu, input_shape=(img_rows * img_cols,)),
@@ -38,8 +48,8 @@ def create_model():
 ])
 
   model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
 
   return model
 
@@ -54,6 +64,6 @@ tb_callback = tf.keras.callbacks.TensorBoard(log_dir=log_path)
 model.fit(x_train, y_train,
           epochs=epochs,
           verbose=1,
-          callbacks=[cp_callback, tb_callback])
+          callbacks=[cp_callback, tb_callback, OpenBayesMetricsCallback()])
 model.save(os.path.join(model_path, model_name))
 print('done')
